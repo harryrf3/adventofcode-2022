21e9ad9e270cdf112c879bbdd7dce7f5eaa59f58
Harry Fahringer III <viccem.dev@gmail.com>
2023-01-28 15:40:45 -0500

    Fetch data from daily url

--- day02/day02.js
+++ day02/day02.js
@@ -1,8 +1,7 @@
-const sampleInput = `vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw`;
 
-const splitInput = sampleInput.split('\n');
+// Grab data from fetch input for daily challenges
+const summonPuzzleInput = async () => {
+  const response = await fetch('https://adventofcode.com/2022/day/2/input');
+  const responseText = await response.text(); // wait for a response
+  return responseText; // return that response
+};
