6c7183af8b786314e4f3c06a60c9bfc7a8bac927
Harry Fahringer III <viccem.dev@gmail.com>
2023-01-29 09:18:11 -0500

    More notes

--- day02/day02.js
+++ day02/day02.js
@@ -4,13 +4,42 @@ const sampleInput =
   C Z`;
 
 const computerChoice = [
-  { A: ROCK },
-  { B: PAPER },
-  { C: SCISSORS }
+  { A: 'ROCK' },
+  { B: 'PAPER' },
+  { C: 'SCISSORS' }
 ];
 
 const playerChoice = [
-  { X: ROCK },
-  { Y: PAPER },
-  { Z: SCISSORS }
+  { X: 'ROCK' },
+  { Y: 'PAPER' },
+  { Z: 'SCISSORS' }
 ];
+
+/* 
+ Points:
+ 1 - Rock
+ 2 - Paper
+ 3 - Scissors
+
+ 0 - Loss
+ 3 - Tie
+ 6 - Win
+*/
+
+/*
+  Losses:
+  1 - 2
+  2 - 3
+  3 - 1
+  
+  Wins:
+  1 - 3
+  2 - 1
+  3 - 2
+*/
+
+for (const round in sampleInput) {
+  // compare each shape in round
+  sampleInput.split(' ');
+  console.log(sampleInput);
+}
