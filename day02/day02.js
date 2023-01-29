9c381c0e16f6ac2f341b9b39daece4b814511390
Harry Fahringer III <viccem.dev@gmail.com>
2023-01-29 08:45:25 -0500

    Add player and computer choices

--- day02/day02.js
+++ day02/day02.js
@@ -3,4 +3,14 @@ const sampleInput =
   B X
   C Z`;
 
-  
+const computerChoice = [
+  { A: ROCK },
+  { B: PAPER },
+  { C: SCISSORS }
+];
+
+const playerChoice = [
+  { X: ROCK },
+  { Y: PAPER },
+  { Z: SCISSORS }
+];
